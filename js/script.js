32fdf5cdddc20abe212758896d0f41bb5f5358eb
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-10-15 16:40:24 +0200

    mod

--- js/script.js
+++ js/script.js
@@ -10,5 +10,5 @@ if ( numero % 2 === 0){ console.log("hai scritto", numero)
 
 else { console.log("Il tuo numero non è pari, scrivo il successivo")
        console.log("Il primo numero pari dopo il tuo è", numero++) 
-       alert( `Il tuo numero non è pari, scrivo il successivo ,${numero}, Il primo numero pari dopo il tuo è  ${numero}++`
+       alert( `Il tuo numero non è pari, scrivo il successivo ,${numero}, Il primo numero pari dopo il tuo è  ${numero++}`
                      }
