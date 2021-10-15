cc2cb4a0acd578136743a885ad26d733f413b84b
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-10-15 17:30:35 +0200

    esercizio due corretto

--- js/scriptSecondo.js
+++ js/scriptSecondo.js
@@ -4,20 +4,56 @@
 const array1 = [15, 2, 7];
 const array2 = [5];
 
+let arrayPiccolo=[];
 
-console.log(array1);
-console.log(array2);
+let arrayGrande=[];
 
-//verifico la lunghezza del secondo rispetto al primo
+//controllo lunghezze array :
 
-while( array2.length<=array1.length){ 
+let lunghezzaArray1=array1.length;
+let lunghezzaArray2 =array2.length;
 
-    //aggiungo un numero random
-    array2.push(Math.ceil(Math.random()*100))
+console.log(lunghezzaArray1);
+console.log(lunghezzaArray2);
 
-    array2.length++;
+if ( lunghezzaArray1===lunghezzaArray2) { console.log("sono uguali")}
+
+else  if (lunghezzaArray1 < lunghezzaArray2){ 
+        arrayPiccolo = array1;
+        arrayGrande  = array2;
+
+        console.log("array1 < array2 ");
+       
+}
+
+else { arrayPiccolo = array2;
+       arrayGrande  = array1;
+    
+       console.log("array1 < array2 ");
+    }
+
+
+//calcolo la differenza
+
+let differenza;
+
+differenza =arrayGrande.length-arrayPiccolo.length;
+
+console.log("la differenza è ", differenza);
+
+
+for ( let i=0; i<differenza; i++) {
+
+    arrayPiccolo.push( Math.ceil(Math.random()*100 ));
+
+    console.log(arrayPiccolo);
+
+     
+
+
 }
 
 
 console.log(array1);
 console.log(array2);
+
