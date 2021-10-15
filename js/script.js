68dc66a818c9b85bb45a35a8525b05cde40aec5a
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-10-15 16:47:31 +0200

    aggiunta validazione

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 // chiedi un numero 
 
-let numero = prompt ("Inserisci un numero pari :")
+let numero = prompt("Inserisci un numero pari :")
 
-if ( numero % 2 === 0){ console.log("hai scritto", numero)
-                        alert( `Hai scritto un numero pari,${numero}`)
-                      }
+numero = parseInt(numero);
+
+if (isNaN(numero)) {
+    alert("non è un numero ricarica la pagina")
+}
+else {
+
+    if (numero % 2 === 0) {
+        console.log("hai scritto", numero)
+        alert(`Hai scritto un numero pari,${numero}`)
+    }
 
 
 
-else { console.log("Il tuo numero non è pari, scrivo il successivo")
-       console.log("Il primo numero pari dopo il tuo è", numero++) 
-       alert( `Il tuo numero non è pari, scrivo il successivo ,${numero}, Il primo numero pari dopo il tuo è  ${numero++}`)
-                     }
+    else {
+        console.log("Il tuo numero non è pari, scrivo il successivo")
+        console.log("Il primo numero pari dopo il tuo è", numero++)
+        alert(`Il tuo numero non è pari, scrivo il successivo ,${numero}, Il primo numero pari dopo il tuo è  ${numero++}`)
+    }
+
+}
