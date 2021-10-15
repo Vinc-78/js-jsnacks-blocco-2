4e22f15ab00585c9e638d0fed99c3e6844666370
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-10-15 16:40:02 +0200

    primo esercizio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// chiedi un numero 
+
+let numero = prompt ("Inserisci un numero pari :")
+
+if ( numero % 2 === 0){ console.log("hai scritto", numero)
+                        alert( `Hai scritto un numero pari,${numero}`)
+                      }
+
+
+
+else { console.log("Il tuo numero non è pari, scrivo il successivo")
+       console.log("Il primo numero pari dopo il tuo è", numero++) 
+       alert( `Il tuo numero non è pari, scrivo il successivo ,${numero}, Il primo numero pari dopo il tuo è  ${numero}++`
+                     }
